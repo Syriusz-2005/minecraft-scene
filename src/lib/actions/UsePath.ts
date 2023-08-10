0a27af0e22e4b47b399531418c847155d2d7f375
Mateusz Słomka <slomkam7@gmail.com>
2023-08-10 16:33:04 +0200

    add use path

---
+++ src/lib/actions/UsePath.ts
@@ -0,0 +1,55 @@
+import ActionTree, { Action, ActionConfig, CompileResult } from "../ActionTree.js";
+import { VMath, Vector } from "../utils/Vector.js";
+
+
+export type PathConfig = {
+  pos: Vector;
+  radius: number;
+  tickSideEffect?: string;
+}
+
+export default class UsePath implements Action {
+  constructor(
+    private pathConfig: PathConfig,
+  ) {}
+
+  public async compile(config: ActionConfig): CompileResult {
+    const {scene} = config;
+    const {pathConfig} = this;
+    let index = config.functionIndex;
+    
+    const {name, reference} = scene.getFunction(config.branchIndex, ++index, 'path-tick');
+
+    await ActionTree.appendAction(config, `
+      function ${reference}
+    `);
+
+    const {name: endFunctionName, reference: endFunctionReference} = scene.getFunction(config.branchIndex, ++index);
+
+    const {name: positionedFunctionName, reference: positionedFunctionRef} = scene.getFunction(config.branchIndex, index, 'positioned-tick');
+
+
+    await scene.mkFile(name, `
+
+      execute positioned ${VMath.toString(pathConfig.pos)} if entity @p[tag=w.player,distance=..${pathConfig.radius}] run function ${endFunctionReference}
+      execute positioned ${VMath.toString(pathConfig.pos)} if entity @p[tag=w.player,distance=..${pathConfig.radius}] run return 1
+
+
+      execute positioned ${VMath.toString(pathConfig.pos)} run function ${positionedFunctionRef}
+
+      schedule function ${reference} 1t
+    `);
+
+    await scene.mkFile(positionedFunctionName, `
+      particle minecraft:glow_squid_ink ~ ~ ~ .1 20 .1 0 10 force
+    `);
+
+    await scene.mkFile(endFunctionName, `
+      #Run after the UsePath
+    `);
+
+    return {
+      endFunctionIndex: index,
+    }
+  }
+}
