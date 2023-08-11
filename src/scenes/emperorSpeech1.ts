0ca8a9bf019ea3d522eb097a7c2a65578b553f9a
Mateusz Słomka <slomkam7@gmail.com>
2023-08-11 19:15:56 +0200

    add additional scripts

--- src/scenes/emperorSpeech1.ts
+++ src/scenes/emperorSpeech1.ts
@@ -6,7 +6,10 @@ import UnfreezePlayer from "../lib/actions/UnfreezePlayer.js";
 import UseCamera from "../lib/actions/UseCamera.js";
 import Wait from "../lib/actions/Wait.js";
 
-
+// commands for switching scenes
+const cleanMarket = 'fill -287 68 -7 -273 68 -24 air'
+const pasteScene = 'clone -278 -4 -8 -292 7 -25 -287 67 -24'
+const pasteMarket = 'clone -294 -4 -8 -308 7 -25 -287 67 -24'
 
 const scene = new Scene({
   NAMESPACED_PATH,
@@ -20,6 +23,19 @@ const scene = new Scene({
 scene.actionTree
   .then('gamemode spectator @a[tag=w.player]')
   .then('execute in overworld run tp @a[tag=w.player] -511 64 13 -116 17.8')
+  .then('fill -287 68 -7 -273 68 -24 air')
+  .then('clone -278 -4 -8 -292 7 -25 -287 67 -24')
+  .then(`
+    summon minecraft:villager ~ ~ ~ {NoAI:true,Silent: true, Tags: ["w.listener"]}
+    summon minecraft:villager -275.6 69 -14.5 {NoAI:true,Rotation:[87f, -7f],Silent:true, Tags: ["w.listener"]}
+    summon minecraft:villager -275.7 69 -21.9 {NoAI:true,Rotation:[50.4f, -5.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -275 69 -15.46 {NoAI:true,Rotation:[92f, -6.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -273.3 69 -16.8 {NoAI:true,Rotation:[110f, -5.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -274.3 69 -16.4 {NoAI:true,Rotation:[110f, -5.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -274 69 -18.5 {NoAI:true,Rotation:[90f, -5.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -276 69 -8.4 {NoAI:true,Rotation:[131f, -5.8f], Tags: ["w.listener"]}
+    summon minecraft:villager -277 69 -11.3 {NoAI:true,Rotation:[110f, -5.8f], Tags: ["w.listener"]}
+    `)
   .then(new FreezePlayer())
   .then('effect give @a[tag=w.player] minecraft:blindness 5 1 true')
   .then(new Wait(.6))
@@ -68,14 +84,32 @@ scene.actionTree
             durationTo: 8,
           },
           {
-            position: [-219, 99, 9],
-            rotation: [135.6, 69.6],
-            durationTo: 3.5,
+            position: [-271, 80.8, -13],
+            rotation: [106, 52],
+            durationTo: 6,
+          }
+        ]
+      }))
+      .then(new UseCamera({
+        anchorPoints: [
+          {
+            position: [-277.7, 68, -12],
+            rotation: [133, -36.9],
+            durationTo: 1,
+          },
+          {
+            position: [-275, 68, -13],
+            rotation: [120, -37],
+            durationTo: 10,
           }
         ]
       }))
   ])
-
+  .then(`tellraw @a "*The speech*"`)
+  .then(new Wait(5))
+  .then(`
+    kill @e[tag=w.listener]
+  `)
 //TODO: scene cleanup
 
 await scene.compile();
