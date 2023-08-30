831b536f44da2dde306134402944dc851a6172e7
Mateusz Słomka <slomkam7@gmail.com>
2023-08-30 14:42:35 +0200

    improve explosion

--- src/scenes/the-mine.ts
+++ src/scenes/the-mine.ts
@@ -19,6 +19,7 @@ const scene = new Scene({
 scene.actionTree
   .then(`
     item replace block -329 10 -112 container.13 with minecraft:flint_and_steel
+    clone -286 21 -94 -281 25 -101 -286 7 -101
   `)
   .then(minerSpeech.sayAs({text: `I'm here to guard you, guys. You can safely destroy the wall.`}, ThePlayer))
   .then(new Wait(3))
@@ -37,6 +38,7 @@ scene.actionTree
     execute positioned -285.30 7.80 -95.25 run kill @e[type=tnt,distance=..40]
     execute positioned -285.30 7.80 -95.25 run function delta:api/explosion_emitter_particle
     clone -286 15 -94 -281 19 -101 -286 7 -101
+    particle minecraft:cloud -282.78 9.00 -97.02 1 1 1 0.2 100
   `)
   
 await scene.compile();
