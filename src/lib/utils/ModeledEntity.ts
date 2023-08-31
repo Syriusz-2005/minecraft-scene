41aaefab6ddb25c6136154c2ce64741d4624e2b0
Mateusz Słomka <slomkam7@gmail.com>
2023-08-31 15:52:58 +0200

    improve walking animation

--- src/lib/utils/ModeledEntity.ts
+++ src/lib/utils/ModeledEntity.ts
@@ -119,9 +119,9 @@ export default class ModeledEntity {
 
       execute as @e[tag=${skeletonEntityTag}] if score @s w.modelId = #temp.modelId w.internal run scoreboard players add #temp.modelCount w.internal 1
 
-      execute store result score @s w.cx run data get entity @s Pos[0] 1000
-      execute store result score @s w.cy run data get entity @s Pos[1] 1000
-      execute store result score @s w.cz run data get entity @s Pos[2] 1000
+      execute store result score @s w.cx run data get entity @s Pos[0] 2000
+      execute store result score @s w.cy run data get entity @s Pos[1] 2000
+      execute store result score @s w.cz run data get entity @s Pos[2] 2000
 
       scoreboard players set #temp.isWalking w.internal 0
 
@@ -132,9 +132,9 @@ export default class ModeledEntity {
       execute if score #temp.isWalking w.internal matches 1 run function animated_java:${modelName}/animations/${walkAnimation}/resume
       execute if score #temp.isWalking w.internal matches 0 run function animated_java:${modelName}/animations/${walkAnimation}/stop
 
-      execute store result score @s w.x run data get entity @s Pos[0] 1000
-      execute store result score @s w.y run data get entity @s Pos[1] 1000
-      execute store result score @s w.z run data get entity @s Pos[2] 1000
+      execute store result score @s w.x run data get entity @s Pos[0] 2000
+      execute store result score @s w.y run data get entity @s Pos[1] 2000
+      execute store result score @s w.z run data get entity @s Pos[2] 2000
 
       execute if score #temp.modelCount w.internal matches 0 run function animated_java:${modelName}/remove/this
       execute if score #temp.modelCount w.internal matches 0 run return 1
