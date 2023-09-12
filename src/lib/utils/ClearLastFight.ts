82069cc29ce8fb2c1b486295a9bde33e369e9202
Mateusz Słomka <slomkam7@gmail.com>
2023-09-12 15:23:32 +0200

    fix a minor bug

--- src/lib/utils/ClearLastFight.ts
+++ src/lib/utils/ClearLastFight.ts
@@ -3,6 +3,6 @@
 
 export const getClearLastFight = (tags: string[]) => `
   gamerule doMobLoot false
-  ${tags.reduce((acc, tag) => acc + `kill @e[tag=${tag}]`, '')}
+  ${tags.reduce((acc, tag) => acc + `\nkill @e[tag=${tag}]`, '')}
   gamerule doMobLoot true
 `
